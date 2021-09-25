706db3400ead309d4d5cd28c570a203a9dfb121f
Artemii Pudovkin <tyumen-777@yandex.ru>
2021-09-25 22:56:24 +0500

    feat: сверстал footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,13 +1,17 @@
 import cn from 'classnames';
 import {FooterProps} from './Footer.props';
 import styles from './Footer.module.css';
+import {format} from 'date-fns';
+import {addYears} from "date-fns/esm/fp";
 
 
-const Footer = ({...props}: FooterProps): JSX.Element => {
+const Footer = ({className, ...props}: FooterProps): JSX.Element => {
     return (
-        <div {...props} >
-            Footer
-        </div>
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+            <a href='#'>Пользовательское соглашение</a>
+            <a href='#'>Политика конфиденциальности</a>
+        </footer>
     );
 };
 
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -9,16 +9,14 @@ import {Component, FunctionComponent} from "react";
 
 export const Layout = ({children}: LayoutProps): JSX.Element => {
     return (
-        <>
-            <Header/>
-            <div>
-                <Sidebar/>
-                <div>
+        <div className={styles.wrapper}>
+            <Header className={styles.header} />
+                <Sidebar className={styles.sidebar} />
+                <div className={styles.body}>
                     {children}
                 </div>
-            </div>
-            <Footer/>
-        </>
+            <Footer className={styles.footer} />
+        </div>
     )
 };
 
